c9e65d4ec7034f9110fe783fbe89d0ded57434a6
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2025-11-14 11:48:42 +0200

    react_tabs-with-router

---
+++ src/components/TabsPage/TabsPage.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Tab } from '../../types/Tab';
+
+interface Props {
+  tabs: Tab[];
+  defaultTabId?: string;
+}
+
+export const TabsPage: React.FC<Props> = ({ tabs, defaultTabId }) => {
+  const params = useParams<{ tabId: string }>();
+  const tabId = params.tabId || defaultTabId || '';
+  const activeTab = tabs.find(t => t.id === tabId);
+
+  return (
+    <>
+      <h1 className="title">Tabs page</h1>
+      <div className="tabs is-boxed">
+        <ul>
+          {tabs.map(tab => (
+            <li
+              key={tab.id}
+              data-cy="Tab"
+              className={tab.id === tabId ? 'is-active' : ''}
+            >
+              <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="block" data-cy="TabContent">
+        {activeTab ? activeTab.content : 'Please select a tab'}
+      </div>
+    </>
+  );
+};
